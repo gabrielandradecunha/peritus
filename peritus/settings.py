e036b9b854f657fd5a8ce86aa1e45d67307814a3
Gabriel Andrade <gandradecortez50@gmail.com>
2026-05-28 14:26:49 -0400

    fix: removendo app desnecessario dashboard

--- peritus/settings.py
+++ peritus/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'usuarios',
     'protocolos',
     'documentos',
-    'dashboard',
 ]
 
 MIDDLEWARE = [
--- usuarios/views.py
+++ usuarios/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate, login, logout
 from .models import Usuario
 
-
 def login_view(request):
     if request.user.is_authenticated:
         return redirect('home')
